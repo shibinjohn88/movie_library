d995efb10b1f1def168fb891c0e6d552e1fcff6f
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-02-08 11:54:20 -0500

    home page created with components Navbar, MovieDisplay, Home

---
+++ movie-library-fe/movie-library/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar () {
+    return (
+        <div className='navbar'>
+            
+            <h1>Movie Library</h1>
+            <h2>Home</h2>
+            <h2>My Favourites</h2>
+            
+        </div>
+    )
+}
+
+export default Navbar
