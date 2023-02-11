c4283b6cba909e4d0a683cae14a305307d480d44
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-02-11 12:41:22 -0500

    favourites page created to display movies added to favourites

--- movie-library-fe/movie-library/src/App.js
+++ movie-library-fe/movie-library/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Show from './components/show.jsx';
 import AddMovie from './components/AddMovie';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FavouritesDisplay from './components/FavouritesDisplay';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/show" element={<Show/>} />
           <Route path="/addmovie" element={<AddMovie/>} />
+          <Route path='/favourites' element={<FavouritesDisplay/>}/>
         </Routes>
       </Router>
     </div>
